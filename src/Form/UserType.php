191781d65d06457c59283d35e45dd95860f99cc2
Thomas Chagneux <Thomas.chagneux@gmail.com>
2025-11-17 14:20:01 +0100

    PP1-11 buildForm params for inspection

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -15,6 +15,11 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     * @return void
+     */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
